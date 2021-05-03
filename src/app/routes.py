96a0db88904609bf55d0307d1ec14f7c00704ad5
BrandNewLifeJackie26 <jackie020130923@126.com>
2021-05-03 14:27:58 -0700

    Create three pages

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'User'}
+    return render_template('index.html', user=user)
+
+@app.route('/analysis')
+def analysis():
+    user = {'username': 'User'}
+    return render_template('analysis.html', user=user)
+
+@app.route('/recommendation')
+def recommendation():
+    user = {'username': 'User'}
+    return render_template('recommendation.html', user=user)
