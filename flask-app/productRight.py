a7bf80e2b69452de76a0c248a28bc895e66b2d8b
BrandNewLifeJackie26 <jackie020130923@126.com>
2021-05-21 23:21:58 -0700

    backend part of demo vega (Altair)

--- flask-app/productRight.py
+++ flask-app/productRight.py
@@ -1,8 +1,18 @@
 import time
 from flask import Flask
 
+import vega_datasets
+import altair as alt
+
 app = Flask(__name__)
 
 @app.route('/time')
 def get_time():
     return {'time': time.time()}
+
+@app.route('/vega')
+def get_vega_js():
+    cars = vega_datasets.data('cars')
+    chart = alt.Chart(cars)
+    chart = chart.mark_point().encode(x='Displacement')
+    return chart.to_json()
